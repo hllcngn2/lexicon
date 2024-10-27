7fd43fe34ba7642026760e6c54757815f08c1cf8
d0pelrh <realhllcngn@gmail.com>
2024-10-27 18:34:24 +0100

    nothing important

--- src/test1.c
+++ src/test1.c
@@ -11,11 +11,13 @@ char *filepath =(char*)malloc(10+strlen(filename)+1);
 strcpy(filepath,"wordlists/");
 strcat(filepath,filename);
 free(filename);
-if(v)	printf("hello1\n");
+
+if(v)	printf("hello1\n");	//debug
 
 int wln; wordlist wl =loadwordsfromfile(&wln, filepath, 0);
 free(filepath);
-if(v){	printf("hello2\n");
+
+if(v){	printf("hello2\n");	//debug
 	printf("wln=%i\n", wln);
 	for (int i=0;i<wln;i++)
 		printf("'%s' %i %i-%i\n",wl[i]->word,wl[i]->type,
@@ -23,14 +25,15 @@ if(v){	printf("hello2\n");
 	fflush(stdout);}
 
 lexic l =construct(wl, wln);
-if(v){	printf("\nhello3");
+
+if(v){	printf("\nhello3");	//debug
 	for (int i=0;i<N_GROUPS;i++){
 	printf("\ngroup no%i:",i);
 	for (int j=0;j<l[i]->n;j++)
 		printf("'%s',",l[i]->list[j]->word);}
 	fflush(stdout);}
 
-printf("-hllcngn's lexicon - test 1-\n");
+printf(" - hllcngn's lexicon - test 1 -\n");
 for (int x=0; x<10; x++){
 char *sentstr =makesentstr(wln, wl, l, v);
 printf("%s ",sentstr);
@@ -38,5 +41,7 @@ free(sentstr);}
 
 freelexic(l);
 freewords(wl, wln);
-if(v){	printf("\nall good\n"); fflush(stdout);}
+
+if(v){	printf("\nall good\n"); fflush(stdout);}	//debug
+
 return 0;}
